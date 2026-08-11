c827162580d6811ea634eb18b80f540f2ae325dc
Chelitox26 <castilloalejandro2626@gmail.com>
2026-08-10 20:07:18 -0600

    controlador get funcion de autor controller

---
+++ GestionCursos/Controllers/AutoresController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BibliotecaMonolito.Models;
+
+namespace BibliotecaMonolito.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AutoresController : ControllerBase
+{
+    private readonly LibraryDbContext _context;
+
+    public AutoresController(LibraryDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var autores = await _context.Autores.ToListAsync();
+        return Ok(autores);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var autor = await _context.Autores.FindAsync(id);
+        if (autor == null)
+            return NotFound();
+
+        return Ok(autor);
+    }
+}
